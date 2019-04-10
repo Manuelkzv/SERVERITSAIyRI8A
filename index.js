f9634c94860065d37a79933bc94f56249b669fcd
Manuel Aguilar <manuel.aguilar@lisder.buap.mx>
2019-04-09 23:08:46 -0500

    Public added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("Public"));
+
+
 app.listen(8080, function(){
 	console.log("Servdor listo");
 });
@@ -8,4 +11,16 @@ app.listen(8080, function(){
 app.get('/', function(req, res){
 	console.log("Conectad al index");
 	res.send("Servidor ITSA IRI 8A Listo");
+});
+
+app.get('/patito', function(req, res){
+	console.log("Conectad al patito");
+  //res.send("Patito listo");
+	res.sendFile(__dirname + "/Public/index.html");
+});
+
+app.get('/Prueba/:algunDato', function(req, res){
+	console.log("Conectad al patito");
+  res.send("Dato leido: " + req.params.algunDato);
+
 });
